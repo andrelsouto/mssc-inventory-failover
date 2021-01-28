59e04acee855b2d77476143c8a384c7509329466
André Luiz <andre.andrade@vsoft.com.br>
2021-01-28 06:24:07 -0300

    Fixing iventory return

--- src/main/java/br/com/andre/msscinventoryfailover/web/InventoryHandler.java
+++ src/main/java/br/com/andre/msscinventoryfailover/web/InventoryHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import java.time.OffsetDateTime;
+import java.util.Arrays;
 import java.util.UUID;
 
 @Component
@@ -16,11 +17,11 @@ public class InventoryHandler {
     public Mono<ServerResponse> listInventory(ServerRequest request) {
         return ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(Mono.just(BeerInventoryDto.builder()
+                .body(Mono.just(Arrays.asList(BeerInventoryDto.builder()
                         .id(UUID.randomUUID())
                         .beerId(UUID.fromString("00000000-0000-0000-0000-000000000000"))
                         .quantityOnHand(999)
-                        .createdDate(OffsetDateTime.now()).build()), BeerInventoryDto.class);
+                        .createdDate(OffsetDateTime.now()).build())), BeerInventoryDto.class);
     }
 
 }
